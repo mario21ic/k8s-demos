3e0b48c25bdef9cabbe414e5f0421094deb6abb1
Mario Inga <mario21ic@gmail.com>
2020-03-02 13:20:08 -0500

    feat(global) adding golang example

--- secrets/golang/main.go
+++ secrets/golang/main.go
@@ -3,11 +3,22 @@ package main
 import (
     "fmt"
     "os"
+    "net/http"
+    "log"
 )
 
-func main() {
-    fmt.Println("hello:", os.Getenv("hello"))
-
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
     //os.Setenv("foo", "1")
     //fmt.Println("foo:", os.Getenv("foo"))
+    responseData := os.Getenv("hello")
+    responseString := string(responseData)
+    fmt.Fprint(w, responseString)
+}
+
+func main() {
+    log.Printf("### func main ###")
+    http.HandleFunc("/", ServeHTTP)
+    if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
--- secrets/golang/start.sh
+++ secrets/golang/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
+image="mario21ic/secret:golang-v1"
 
-docker build -t mario21ic/secret:golang-v1 .
-docker run mario21ic/secret:golang-v1
-docker run -e hello="mario21ic" mario21ic/secret:golang-v1
+docker build -t $image .
+
+docker run -d -p 8080:8080 $image
+sleep 2
+curl localhost:8080
+
+docker run -d -e hello="mario21ic" -p 8081:8080 $image
+sleep 2
+curl localhost:8081
